5f4cdd036f26994d01c442d5958f8fefa714c046
🐶 Orr Benyamini <orrb@amazon.com>
2024-05-28 18:21:35 +0300

    Initial Commit

---
+++ GettingStarted/GettingStartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedApp.swift
+//  GettingStarted
+//
+//  Created by Benyamini, Orr on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
