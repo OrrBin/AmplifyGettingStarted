a869de748e96df919157f736e8eaf7608360b6a3
🐶 Orr Benyamini <orrb@amazon.com>
2024-05-29 13:12:17 +0300

    Working notes app

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "gettingstarted": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "gettingstarted15884a3e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "image": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
